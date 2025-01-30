6de2f46f22b8d46e8315b40aff5e69100c17c6d8
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-01-30 10:52:02 -0600

    This is the initial setup of my project 2

---
+++ scripts/myScripts.js
@@ -0,0 +1,37 @@
+//Global variables
+let selectedGender = "";
+let selectType ="";
+
+//Generate Contact function
+function addContact() {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+
+    //validate inputs
+    if (name === "" || phone === "" || selectType === ""|| selectedGender === "") {
+        alert("Please enter name, phone number, contact type, and gender.");
+        return;
+    }
+
+    //create a new list of contacts
+    let li = document.createElement("li");
+    li.textContent = `${"Name: " + name} - ${"Phone: " + phone} - ${"Contact Type: " + selectType} - 
+    (${"Gender: " + selectedGender})`;
+
+    //starts adding contacts to the list
+    document.getElementById("contactList").appendChild(li);
+
+    //clear inputs
+    selectedGender = "";
+    selectType ="";
+}
+function setGender(gender) {
+    selectedGender = gender;
+    document.getElementById("maleButton");
+    document.getElementById("femaleButton");
+}
+function setType(type){
+    selectType = type;
+    document.getElementById("personalButton");
+    document.getElementById("workButton");
+}
